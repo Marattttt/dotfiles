89ec084989319f912029793cf109781bb5f4b9a2
Marat Bakasov <bakasovmarat04@gmail.com>
2025-07-02 11:00:37 +0600

    neovim: remove double lsp activation, nushell: remove shell history :)

--- .config/nvim/lua/scripts/setuplsp.lua
+++ .config/nvim/lua/scripts/setuplsp.lua
@@ -1,8 +1,9 @@
+-- Mason seems to set up LSPs automatically
+
 -- Automatically set up LSP servers
-local lspconfig = require("lspconfig")
-local servers = require("mason-lspconfig").get_installed_servers()
-
-for i, server_name in pairs(servers) do
-  lspconfig[server_name].setup({})
-end
-
+-- local lspconfig = require("lspconfig")
+-- local servers = require("mason-lspconfig").get_installed_servers()
+-- for i, server_name in pairs(servers) do
+--   lspconfig[server_name].setup({})
+-- end
+--
